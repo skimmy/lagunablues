10b705ccdb384339f89373e4d7094db3a6f4003f
Michele Schimd <skimmoz@gmail.com>
2023-08-30 18:41:53 +0200

    Added ribbon to sale item.
    We changed the layout of SellItem which now have a ribbon indicating
    the amount of discount (which is hidden if no discount is available).

--- lagunablues/src/app/festival/merch/sellitem.js
+++ lagunablues/src/app/festival/merch/sellitem.js
@@ -1,5 +1,15 @@
 import { pubPath } from "@/lib/util";
 
+function Ribbon({children}) {
+    return (
+        <div className="relative">
+            <div className="absolute position-fixed bg-laguna-red font-bold px-3 py-1 top-0 right-[-15px] rotate-45">
+                {children}
+            </div>
+        </div>
+    );
+}
+
 function ItemPrice({item, currency="€"}) {
     var discount = 0;
     if (item.discountPrice && item.price > item.discountPrice) {
@@ -7,15 +17,20 @@ function ItemPrice({item, currency="€"}) {
     }
     return (
         <p>
-            { discount > 0 ? <span className="bg-laguna-yellow text-black w-fill mr-2">Save {discount}% </span> : ""}
             <span className="text-2xl w-fill mt-1">{discount ? item.discountPrice : item.price} {currency}</span>
+            { discount > 0 ? <span className="pl-2 text-xl line-through w-fill mt-1 text-gray-400">{item.price} € </span> : ""}
         </p>
     );
 }
 
 export default function SellItem({item}) {
+    var discount = 0;
+    if (item.discountPrice && item.price > item.discountPrice) {
+        discount = Math.round(item.discountPrice / item.price * 100);
+    }
     return(
         <div className="shadow-md shadow-gray-600 hover:shadow-gray-100">
+        {discount > 0 ? <Ribbon>-{discount}%</Ribbon> : ""}
             <div className="w-fill bg-gray-800 p-4">
                 <a href={item.detail_url || "#"}><img className="w-[150px] h-[150px]" src={item.thumbnail ? item.thumbnail : pubPath("/merch/placeholder.svg")}></img></a>
             </div>
