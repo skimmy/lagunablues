871c05366f72ce5b0f7ab5d6557f056585bf5a1f
Michele Schimd <skimmoz@gmail.com>
2023-08-30 14:56:58 +0200

    Improvements on the selling item component.
    Improved the layout of the component and added a ItemPrice
    sub-component that controls and adjusts the selling price based on
    the presence of a 'discountPrice' attribute on the input item.

--- lagunablues/src/app/festival/merch/page.js
+++ lagunablues/src/app/festival/merch/page.js
@@ -1,19 +1,20 @@
 import SellItem from "./sellitem";
 
 const items = [
-    { name: "CD 2023", price: "15€", thumbnail: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/CD_icon_test.svg/1200px-CD_icon_test.svg.png" },
-    { name: "Women T-shirt", price: "16€" },
-    { name: "Man T-shirt", price: "16€", thumbnail: "https://images.prismic.io/rushordertees-web/e225cda8-d94c-4f4a-8bef-d0ddbdd506a3_Performance+Shirts.jpg?auto=compress,format&rect=0,0,1600,1800&w=800&h=900" },
-    { name: "Unisex jumper", price: "22€" },
+    { name: "CD 2023", price: "15", discountPrice: "10", thumbnail: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/CD_icon_test.svg/1200px-CD_icon_test.svg.png", available: "5" },
+    { name: "Women T-shirt", price: "16", available: "8" },
+    { name: "Man T-shirt", price: "16", thumbnail: "https://images.prismic.io/rushordertees-web/e225cda8-d94c-4f4a-8bef-d0ddbdd506a3_Performance+Shirts.jpg?auto=compress,format&rect=0,0,1600,1800&w=800&h=900", available: "2" },
+    { name: "Unisex jumper", price: "22", available: "0" },
 ]
 
 export default function FestivalMerch() {
     return (
         <div>
             <h1 className="text-center my-4 text-4xl md:text-6xl text-laguna-red">Laguna<span className="text-laguna-yellow" style={{textShadow: "1px 1px #000000"}}>Blues</span> Shop</h1>
-            <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 mt-8">
+            <div className="grid grid-cols-1 gap-0 md:grid-cols-2 xl:grid-cols-4 mt-8">
                 {items.map((item, index) => (
-                    <div className="place-self-center mb-4 p-6 border border-double border-2 hover:bg-red-950">
+                    // <div className="place-self-center mb-4 p-6 border border-double border-2 hover:bg-red-950">
+                    <div className="place-self-center">
                         <SellItem key={index} item={item}></SellItem>
                     </div>
                 ))}
--- lagunablues/src/app/festival/merch/sellitem.js
+++ lagunablues/src/app/festival/merch/sellitem.js
@@ -1,11 +1,33 @@
 import { pubPath } from "@/lib/util";
 
+function ItemPrice({item, currency="€"}) {
+    var discount = 0;
+    if (item.discountPrice && item.price > item.discountPrice) {
+        discount = Math.round(item.discountPrice / item.price * 100);
+    }
+    return (
+        <p>
+            { discount > 0 ? <span className="bg-laguna-yellow text-black w-fill mr-2">Save {discount}% </span> : ""}
+            <span className="text-2xl w-fill mt-1">{discount ? item.discountPrice : item.price} {currency}</span>
+        </p>
+    );
+}
+
 export default function SellItem({item}) {
     return(
-        <div>
-            <img className="w-[150px] h-[150px]" src={item.thumbnail ? item.thumbnail : pubPath("/merch/placeholder.svg")}></img>
-            <p>Item: {item.name}</p>
-            <p>Price: {item.price}</p>
+        <div className="shadow-md shadow-gray-600 hover:shadow-gray-100">
+            <div className="w-fill bg-gray-800 p-4">
+                <a href={item.detail_url || "#"}><img className="w-[150px] h-[150px]" src={item.thumbnail ? item.thumbnail : pubPath("/merch/placeholder.svg")}></img></a>
+            </div>
+            <div className="bg-gray-900 w-fill p-4">
+                <p className=""><a className="hover:text-laguna-yellow" href={item.detail_url || "#"}>{item.name}</a></p>
+                {item.available > 0 ?
+                    <p className={`text-sm ${item.available > 3 ? "text-green-600" : "text-laguna-red"}`}>{item.available > 3 ? "" : "Only "}{item.available} Available</p> :
+                    <p className="text-sm text-laguna-red">Out of stock</p>
+                    
+                }
+                <ItemPrice item={item}></ItemPrice>
+            </div>
         </div>
     );
 }
